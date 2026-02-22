71ef851dbc8a53a3ae728af982d530120cc76092
Kshitiz Prakash <kshitizsrivastav3344@gmail.com>
2026-02-22 14:28:02 +0530

    disabled next cache for vercel

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -38,7 +38,9 @@ export default function Home() {
 
   const fetchData = async () => {
     try {
-      const res = await fetch('https://portfolio-dashboard-csw1.onrender.com/api/portfolio');
+      const res = await fetch('https://portfolio-dashboard-csw1.onrender.com/api/portfolio', {
+        cache: 'no-store'
+      });
       if (!res.ok && res.status !== 202) throw new Error('API fetch error');
 
       if (res.status === 202) {
